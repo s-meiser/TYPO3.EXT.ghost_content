abbda80894ceae1a4862ca3b37808fbf1ae96ee5
r3h6 <r3h6@outlook.com>
2017-04-18 23:27:44 +0200

    [TASK] Update templates

--- Classes/Hooks/PageHook.php
+++ Classes/Hooks/PageHook.php
@@ -26,13 +26,11 @@ use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use R3H6\GhostContent\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Backend\View\PageLayoutView;
 use TYPO3\CMS\Core\Utility\StringUtility;
-use TYPO3\CMS\Frontend\Page\PageRepository;
 
 /**
  * hook to display the evaluation results in the page module
  *
  * Class pageHook
- * @package Clickstorm\CsSeo\Hook
  */
 class PageHook
 {
@@ -43,15 +41,11 @@ class PageHook
      */
     protected $extensionConfiguration;
 
-    /**
-     * @var \TYPO3\CMS\Frontend\Page\PageRepository
-     */
-    protected $pageRepository;
+    protected $pageRenderer;
 
     public function __construct()
     {
         $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
-        $this->pageRepository = GeneralUtility::makeInstance(PageRepository::class);
     }
 
     /**
@@ -104,16 +98,15 @@ class PageHook
     {
         if ((int)$parentObject->MOD_SETTINGS['function'] === 1) {
 
-            $this->loadCss();
-            $this->loadJavascript();
-
-            // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($parentObject);
-            // \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($GLOBALS['BE_USER']);
 
             $validColPos = GeneralUtility::intExplode(',', $parentObject->activeColPosList, true);
             $validColPos = array_merge($validColPos, GeneralUtility::intExplode(',', $this->extensionConfiguration->get('whiteList'), true));
 
-            $ghosts = $this->findGhosts($parentObject->pageinfo['uid'], $parentObject->current_sys_language, $validColPos);
+            $ghosts = $this->findGhosts($parentObject->pageinfo['uid'], $validColPos);
+
+            $this->loadCss();
+            $this->loadJavascript();
+
             foreach ($ghosts as &$ghost) {
                 $urlParameters = [
                     'edit' => [
@@ -150,34 +143,17 @@ class PageHook
 
         $view->setLayoutRootPaths($layoutPaths);
         $view->setPartialRootPaths($partialPaths);
-        $view->setTemplatePathAndFilename($absoluteResourcesPath . 'Private/Templates/PageHook.html');
+        $view->setTemplatePathAndFilename($absoluteResourcesPath . 'Private/Templates/PageHook/Render.html');
 
         return $view;
     }
 
-    protected function findGhosts($pageUid, $language, array $validColPos)
+    protected function findGhosts($pageUid, array $validColPos)
     {
         $where = 'pid='.(int) $pageUid;
         $where .= ' AND colPos NOT IN('.join(',', $validColPos).')';
         $where .= ' AND deleted=0'; // Only enable field we have to take care off in be mode!
-        $where .= ' AND sys_language_uid='.$language;
-
-        $records =  $this->getDatabaseConnection()->exec_SELECTgetRows('*', 'tt_content', $where);
-
-        if ($GLOBALS['BE_USER']->workspace) {
-            foreach ($records as &$record) {
-                BackendUtility::workspaceOL('tt_content', $record);
-                // $this->pageRepository->init(true);
-                // $this->pageRepository->versioningPreview = true;
-                // $this->pageRepository->versioningWorkspaceId = $GLOBALS['BE_USER']->workspace;
-                // $this->pageRepository->versionOL('tt_content', $record);
-                // if (is_array($record)) {
-                //     $records[$key] = $record;
-                // }
-            }
-        }
-
-        return array_filter($records);
+        return $this->getDatabaseConnection()->exec_SELECTgetRows('*', 'tt_content', $where);
     }
 
     /**
